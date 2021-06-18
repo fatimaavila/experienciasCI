4ac94caab30313c46669e55b8e7a5db7f0cbb578
vanexperiences <85986014+vanexperiences@users.noreply.github.com>
2021-06-18 18:36:07 +0200

    Update helpers.js

--- backend/helpers.js
+++ backend/helpers.js
@@ -1,13 +1,1 @@
-'use strict';
-const { format } = require('date-fns');
 
-// Formatear un objeto fecha al formato DATETIME de SQL.
-function formatDate(date) {
-    return format(date, 'yyyy-MM-dd HH:mm:ss');
-}
-
-async function deletePhoto(photoName) {
-    const photoPath = path.join(uploadsDir, photoName);
-    await unlink(photoPath);
-}
-module.exports = { formatDate };
