76c5f5d9eb9a0f384a3a0e309db84c6625cf6fe3
hatashi199 <alejandromf_199@hotmail.com>
2021-06-21 12:26:43 +0200

    Endpoints Comprobados

--- backend/controllers/users/editUser.js
+++ backend/controllers/users/editUser.js
@@ -10,8 +10,8 @@ const editUser = async (req, res, next) => {
         connection = await getDB();
 
         const { idUser } = req.params;
-        const { email, ccc, address, phone, bio, cp } = req.body;
-        const avatar = req.files;
+        let { email, ccc, address, phone, bio, cp } = req.body;
+        let avatar = req.files;
 
         const now = new Date();
 
@@ -37,38 +37,61 @@ const editUser = async (req, res, next) => {
             throw error;
         }
 
-        const [userEmail] = await connection.query(
-            `
-            SELECT id FROM users WHERE email = ?;
-        `,
-            [email]
-        );
-
-        if (userEmail.length > 0) {
-            const error = new Error('Ya existe un usuario con ese email');
-            error.httpStatus = 401;
-            throw error;
-        }
-
-        const [userTel] = await connection.query(
-            `
-            SELECT telefono FROM users WHERE email = ?;
-        `,
-            [email]
-        );
-
-        if (userTel.length > 0) {
-            const error = new Error('Ya existe un usuario con ese teléfono');
-            error.httpStatus = 401;
-            throw error;
-        }
-
         const [user] = await connection.query(
             `
-            SELECT avatar FROM users WHERE id = ?;
+            SELECT id, email, ccc, direccion, telefono, bio, cp, avatar FROM users WHERE id = ?;
         `,
             [idUser]
         );
+
+        email = !email ? user[0].email : email;
+        ccc = !ccc ? user[0].ccc : ccc;
+        address = !address ? user[0].direccion : address;
+        phone = !phone ? user[0].telefono : phone;
+        bio = !bio ? user[0].bio : bio;
+        cp = !cp ? user[0].cp : cp;
+        avatar = !avatar ? user[0].avatar : avatar;
+
+        if (email && email !== user[0].email) {
+            const [existingEmail] = await connection.query(
+                `SELECT id FROM users WHERE email = ?;`,
+                [email]
+            );
+
+            if (existingEmail.length > 0) {
+                const error = new Error(
+                    'Ya existe un usuario con ese email'
+                );
+                error.httpStatus = 409;
+                throw error;
+            }
+
+            await connection.query(
+                `UPDATE users SET email = ?, modifiedAt = ? WHERE id = ?;`,
+                [email, formatDate(now), idUser]
+            );
+        }
+
+        if (phone && phone !== user[0].phone) {
+           
+            const [existingPhone] = await connection.query(
+                `SELECT id FROM users WHERE telefono = ?;`,
+                [phone]
+            );
+
+            if (existingPhone.length > 0) {
+                const error = new Error(
+                    'Ya existe un usuario con ese telefono'
+                );
+                error.httpStatus = 409;
+                throw error;
+            }
+
+            await connection.query(
+                `UPDATE users SET telefono = ?, modifiedAt = ? WHERE id = ?;`,
+                [phone, formatDate(now), idUser]
+            );
+        }
 
         if (avatar) {
             if (user[0].avatar) {
@@ -84,14 +107,12 @@ const editUser = async (req, res, next) => {
                 [avartarName, formatDate(now), idUser]
             );
         }
-
+        
         await connection.query(
             `
-            UPDATE users SET email = ? OR ccc = ? OR direccion = ? OR 
-                telefono = ? OR bio = ? OR cp = ? OR modifiedAt = ?
-                WHERE id = ?;
+            UPDATE users SET ccc = ?, direccion = ?, bio = ?, cp = ?, modifiedAt = ? WHERE id = ?;
         `,
-            [email, ccc, address, phone, bio, cp, formatDate(now), idUser]
+            [ccc, address, bio, cp, formatDate(now), idUser]
         );
 
         res.status(200).send({
