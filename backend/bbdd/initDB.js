6acd75b9d81bea6cd398497ab7436102d40c4f93
Nacho Rodriguez Sanz <nachors@MBP-de-Nacho.home>
2021-06-22 17:04:09 +0200

    2

--- backend/bbdd/initDB.js
+++ backend/bbdd/initDB.js
@@ -58,7 +58,7 @@ const initDB = async () => {
             precio DECIMAL(5, 2) NOT NULL,
             categorias VARCHAR(25) NOT NULL,
             num_participantes INT NOT NULL,
-            disp BOOLEAN NOT NULL,
+            disp BOOLEAN DEFAULT 1 NOT NULL,
             fecha_inicio DATE NOT NULL,
             fecha_fin DATE NOT NULL
         );
