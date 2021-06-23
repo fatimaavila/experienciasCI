3881df29cdf40d7207fbb4f7f6641b180eea6a6c
Nacho Rodriguez Sanz <nachors@MBP-de-Nacho.home>
2021-06-23 17:34:04 +0200

    validationsComments

---
+++ backend/validations/newSchemaComment.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+
+const newSchemaComment = Joi.object().keys({
+    comment: Joi.string()
+        .required()
+        .min(15)
+        .max(600)
+        .error((errors) => {
+            switch (errors[0].code) {
+                case 'any.required':
+                    return new Error(
+                        'El campo comentario no puede estar vacio'
+                    );
+                case 'string.max':
+                    return new Error(
+                        'El campo comentario no contener más de 600 caracteres'
+                    );
+                case 'string.min':
+                    return new Error(
+                        'El campo comentario no contener menos de 15 caracteres'
+                    );
+
+                default:
+                    return new Error('El comentario no valido.');
+            }
+        }),
+});
+
+module.exports = { newSchemaComment };
