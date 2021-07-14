5e073ffd974a1ec620dc2bee604484c6f762f21c
NachoRS <nachorsanz@gmail.com>
2021-07-14 16:28:47 +0200

    categoryGrid y cambios estruc landing

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -3,18 +3,20 @@ import styled from 'styled-components';
 import GridCategory from '../../components/gridcategories/GridCategory';
 
 const Landing = styled.div`
-  display: flex;
-  aling-items: center;
-  justify-content: center;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1px, 1fr, 1em;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
   justify-content: center;
   text-align: center;
   aling-items: center;
-  width: 70%;
+`;
+
+const Container = styled.div`
+  grid-area: 2 / 3;
+
+  text-align: center;
+  aling-items: center;
 `;
 
 function Home() {
