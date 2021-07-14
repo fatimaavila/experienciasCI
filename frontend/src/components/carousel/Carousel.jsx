2f6a61f774752ba4ea06b2d37ecac9bc1474e748
vicenteas <vaszm1996@gmail.com>
2021-07-14 14:49:33 +0200

    ya

--- frontend/src/components/carousel/Carousel.jsx
+++ frontend/src/components/carousel/Carousel.jsx
@@ -4,6 +4,7 @@ import Slider from 'infinite-react-carousel';
 const Carousel = ({images}) => {
     return (
         <Slider>
+            <div></div>
             images.map(image)
         </Slider>
     )
