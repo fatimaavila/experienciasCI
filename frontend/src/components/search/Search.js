a81a713d97917531686cd4e2597b9e022480fec2
hatashi199 <alejandromf_199@hotmail.com>
2021-07-28 12:15:11 +0200

    Search Finished

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,7 +1,6 @@
 import Button from '../button/Button';
 import BlackTransparentBox from '../BlackTransparentBox/BlackTransparentBox';
 import StyledLandingSearch from './StyledLandingSearch';
-
 import Input from '../input/Input';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -16,32 +15,21 @@ function Search() {
   const [search, setSearch] = useState(INITIAL_VALUES);
 
   function searchData() {
-    let path = '/experiences';
+
+    let path = `/experiences${search.experience ? `?searchExp=${search.experience}${search.city && `&searchCity=${search.city}`}` : `?searchCity=${search.city}${search.experience && `&searchExp=${search.experience}`}`}`;
     history.push(path);
   }
+
+  console.log(search.experience);
 
   return (
     <StyledLandingSearch className="posRel searchHome">
       <BlackTransparentBox>
-        <Input
-          type="text"
-          placeholder="Introduce una experiencia"
-          querySearch={search.experience}
-          value={search.experience}
-          onChange={(e) => setSearch({ ...search, experience: e.target.value })}
-        />
+        <Input type='text' placeholder="Introduce una experiencia" querySearch={search.experience} value={search.experience} onChange={(e) => setSearch({...search,experience: e.target.value})}/>
 
-        <Input
-          type="text"
-          placeholder="Introduce una ciudad"
-          querySearch={search.city}
-          value={search.city}
-          onChange={(e) => setSearch({ ...search, city: e.target.value })}
-        />
+        <Input type='text' placeholder="Introduce una ciudad" querySearch={search.city} value={search.city} onChange={(e) => setSearch({...search,city: e.target.value})}/>
 
-        <Button blue onClickButton={searchData}>
-          BUSCAR
-        </Button>
+        <Button blue onClickButton={searchData}>BUSCAR</Button>
       </BlackTransparentBox>
     </StyledLandingSearch>
   );
